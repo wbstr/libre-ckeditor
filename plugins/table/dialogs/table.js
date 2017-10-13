64ce028df2f1d9af6505c4c59d7d5ed005b2ec12
Lali <balazs.lajos@webstar.hu>
2017-10-13 10:50:12 +0200

    alapból 100% széles legyen a táblázat

--- plugins/table/dialogs/table.js
+++ plugins/table/dialogs/table.js
@@ -377,7 +377,7 @@
 								label: editor.lang.common.width,
 								title: editor.lang.common.cssLengthTooltip,
 								// Smarter default table width. (http://dev.ckeditor.com/ticket/9600)
-								'default': editor.filter.check( 'table{width}' ) ? ( editable.getSize( 'width' ) < 500 ? '100%' : 500 ) : 0,
+								'default': '100%',
 								getValue: defaultToPixel,
 								validate: CKEDITOR.dialog.validate.cssLength( editor.lang.common.invalidCssLength.replace( '%1', editor.lang.common.width ) ),
 								onChange: function() {
