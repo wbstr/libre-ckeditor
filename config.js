f47c827438d4e599b9f74cb08228980e8d433b81
Lali <balazs.lajos@webstar.hu>
2017-10-13 10:37:58 +0200

    width attributumot ne transzformáljuk style attribútummá

--- config.js
+++ config.js
@@ -4,10 +4,8 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	
-	// %REMOVE_START%
-	// The configuration options below are needed when running CKEditor from source files.
 	config.plugins = 'basicstyles,notification,button,toolbar,clipboard,panel,floatpanel,menu,contextmenu,indent,indentlist,list,resize,showborders,dialogui,dialog,table,tabletools,tableselection,undo,wysiwygarea,base64image,justify';
-	config.skin = 'moono-lisa';
-	// %REMOVE_END%
+	config.skin = 'moono-lisa',
+	config.extraAllowedContent = 'table[width]',
+	config.disallowedContent = 'table{width}';
 };
