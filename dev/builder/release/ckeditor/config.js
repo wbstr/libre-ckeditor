f0d6343b813be12f550b78b4e80e250fb1357e8b
Lali <balazs.lajos@webstar.hu>
2017-10-31 15:03:20 +0100

    #40059 - release

---
+++ dev/builder/release/ckeditor/config.js
@@ -0,0 +1,6 @@
+﻿/*
+ Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.editorConfig=function(a){a.plugins="dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,wsc,base64image,blockimagepaste";
+a.skin="moono-lisa";a.extraAllowedContent="table[width]";a.disallowedContent="table{width}"};
---
+++ dev/builder/release/ckeditor/plugins/blockimagepaste/README.md
@@ -0,0 +1,29 @@
+# blockimagepaste
+Block drag &amp; drop image OR direct image paste into CKEditor
+
+
+# Adding it to CKEdtior
+
+    cd <ckeditor>
+    cd plugins/
+    git clone https://github.com/coolshou/blockimagepaste.git
+
+Now add the plugin in your config.js
+
+    cd <ckeditor>
+    nano config.js
+
+for redmine_ckeditor
+
+    cd <redmine>
+    cd plugins/redmine_ckeditor/assets/ckeditor-contrib/plugins
+    git clone https://github.com/coolshou/blockimagepaste.git
+    cd plugins/redmine_ckeditor/assets/ckeditor
+    nano config.js
+
+with following setting
+
+    CKEDITOR.editorConfig = function( config )
+    {
+     config.extraPlugins = "blockimagepaste";
+    };
