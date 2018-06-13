f1b92e5261c3abf98972dbb0a2cb25e6219ac9cb
Lali <balazs.lajos@webstar.hu>
2018-06-13 11:19:38 +0200

    release könyvtár ne legyen a git-ben

--- dev/builder/release/ckeditor/config.js
+++
@@ -1,6 +0,0 @@
-﻿/*
- Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-CKEDITOR.editorConfig=function(a){a.plugins="dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,wsc,base64image,blockimagepaste,imageresize";
-a.skin="moono-lisa";a.disableObjectResizing=!0;a.extraAllowedContent="table[width];p{margin-top,margin-bottom}";a.disallowedContent="table{width}"};
