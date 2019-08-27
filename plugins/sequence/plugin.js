0f75f55ecf2226c88c68f2dd58b88aeaa095fd4e
dave <udvardi.david@webstar.hu>
2019-08-27 14:19:38 +0200

    refs #57627 - CkEditorban drag&drop esetén újraosztja a data-id -kat, ami átláthatalan változás mutatáshoz vezet.

--- plugins/sequence/plugin.js
+++ plugins/sequence/plugin.js
@@ -1,6 +1,8 @@
 CKEDITOR.plugins.add('sequence', {
 
     init: function (editor) {
+        var dataIds = [];
+
         editor.sequence = {
             _value: 0,
 
@@ -11,48 +13,55 @@ CKEDITOR.plugins.add('sequence', {
         };
 
         var tagWithDataId = /(<[^>]*data-id=")([0-9]*)("[^>]*>)/gm;
+        var tableOrImgRegex = /(<table|<img)([^>]*>)/gm;
 
         function updateOrCreateId(html) {
-            var tableOrImgRegex = /(<table|<img)([^>]*>)/gm;
             return html.replace(tableOrImgRegex, function (full, tag, afterTag) {
                 var nextId = editor.sequence.next(editor);
 
                 var match = tagWithDataId.exec(full);
                 tagWithDataId.lastIndex = 0;
                 if (match) {
+                    dataIds.push(nextId);
                     return match[1] + nextId + match[3];
                 }
 
+                dataIds.push(nextId);
                 return tag + ' data-id="' + nextId + '" ' + afterTag;
             });
         }
 
-        function chkId() {
-            // don't execute code if the editor is readOnly
-            if (editor.readOnly)
-                return;
+        function checkDataIds(html) {
+            dataIds.length = 0;
+            return html.replace(tableOrImgRegex, function (full) {
+                var match = tagWithDataId.exec(full);
+                tagWithDataId.lastIndex = 0;
 
-            setTimeout(function () {
-                editor.document.$.body.innerHTML = updateOrCreateId(editor.document.$.body.innerHTML);
-            }, 100);
+                if (match) {
+                    var id = parseInt(match[2], 10);
+
+                    if (dataIds.includes(id)) {
+                        return updateOrCreateId(full);
+                    }
+
+                    dataIds.push(id);
+                    return full;
+                } else {
+                    return updateOrCreateId(full);
+                }
+            });
         }
-
-        editor.on('contentDom', function () {
-            // For Firefox
-            editor.document.on('drop', chkId);
-            // For IE
-            editor.document.getBody().on('drop', chkId);
-        });
 
         editor.on('paste', function (e) {
             var html = e.data.dataValue;
-            if (!html)
+            var match = tableOrImgRegex.exec(html);
+            if (!html || !match)
                 return;
             e.data.dataValue = updateOrCreateId(html);
         });
 
         editor.on('setData', function (e) {
-            var html = updateOrCreateId(e.data.dataValue);
+            var html = checkDataIds(e.data.dataValue);
             var sequence = editor.sequence._value;
 
             var match;
